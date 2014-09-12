656823ecb19c7043d8a691a617cbedeea5e9ad6d
Nathan Clarke <CaptainCow95@gmail.com>
2014-09-12 10:58:50 -0600

    Basic Logger
    Have a basic logging class.

---
+++ MeshNetwork/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace MeshNetwork
+{
+    internal class Logger
+    {
+        private string _filename;
+
+        public Logger(string filename)
+        {
+            _filename = filename;
+        }
+
+        public void Write(string message)
+        {
+            using (StreamWriter file = new StreamWriter(_filename, true))
+            {
+                file.WriteLine(String.Concat(DateTime.UtcNow.ToString("M/d/yyyy HH:mm:ss"), ": ", message));
+            }
+        }
+    }
+}
